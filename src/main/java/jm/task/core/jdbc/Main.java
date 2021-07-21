27e5da2f9fe17ce13fe5f6abd61a8acd4576cdf4
VladiMun <mynvladimir@gmail.com>
2021-07-21 22:17:47 +0900

    I hope final one)

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+        Util util = new Util();
+        util.getConnection();
+
+        UserServiceImpl test = new UserServiceImpl();
+
+        test.createUsersTable();
+
+        test.saveUser("Vladi", "Mun", (byte) 34);
+        test.saveUser("Vero", "Mun", (byte) 34);
+        test.saveUser("Andrew", "Mun", (byte) 9);
+        test.saveUser("Simon", "Mun", (byte) 3);
+
+        System.out.println(test.getAllUsers().toString());
+
+        test.cleanUsersTable();
+
+        test.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,165 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+public class UserServiceImpl extends Util implements UserService {
 
+    public void createUsersTable() throws SQLException {
+
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = null;
+
+        String sql = "CREATE TABLE USER(id BIGINT, name VARCHAR(255), lastName VARCHAR(255), age INT)";
+
+        try{
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeUpdate();
+        } catch (SQLException ignored) {
+        }finally {
+            if (preparedStatement != null){
+                preparedStatement.close();
+            }
+            if (connection != null){
+                connection.close();
+            }
+        }
     }
 
-    public void dropUsersTable() {
+    public void dropUsersTable() throws SQLException {
 
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = null;
+
+        String sql = "DROP TABLE USER";
+
+        try{
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeUpdate();
+        } catch (SQLException ignored) {
+        }finally {
+            if (preparedStatement != null){
+                preparedStatement.close();
+            }
+            if (connection != null){
+                connection.close();
+            }
+        }
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
 
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = null;
+
+        String sql = "INSERT INTO USER (name, lastName, age) VALUE (?, ?, ?)";
+
+        try {preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+
+            preparedStatement.executeUpdate();
+
+            System.out.println("User with the name of " + name + " added in the table");
+
+        }catch (SQLException ignored){
+        }finally {
+            if (preparedStatement != null){
+                preparedStatement.close();
+            }
+            if (connection != null){
+                connection.close();
+            }
+        }
     }
 
-    public void removeUserById(long id) {
+    public void removeUserById(long id) throws SQLException {
 
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = null;
+
+        String sql = "DELETE FROM USER WHERE ID=?";
+
+        User user = new User();
+
+        try{
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException ignored) {
+        }finally {
+            if (preparedStatement != null){
+                preparedStatement.close();
+            }
+            if (connection != null){
+                connection.close();
+            }
+        }
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    public List<User> getAllUsers() throws SQLException {
+
+        Connection connection = getConnection();
+        List<User> userList = new ArrayList<>();
+
+        String sql = "SELECT id, name, lastName, age FROM USER";
+
+        Statement statement = null;
+
+        try {
+            statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()){
+                User user = new User();
+                user.setId(resultSet.getLong("ID"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+
+                userList.add(user);
+            }
+
+        } catch (SQLException ignored) {
+        }finally {
+            if (statement != null){
+                statement.close();
+            }
+            if (connection != null){
+                connection.close();
+            }
+        }
+
+        return userList;
     }
 
-    public void cleanUsersTable() {
+    public void cleanUsersTable() throws SQLException {
 
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = null;
+
+        String sql = "DELETE FROM USER";
+
+        try{
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.executeUpdate();
+        } catch (SQLException ignored) {
+        }finally {
+            if (preparedStatement != null){
+                preparedStatement.close();
+            }
+            if (connection != null){
+                connection.close();
+            }
+        }
     }
 }
